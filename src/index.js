107929c64e8e6e13f403c494cfdac62afbed5e2d
Josue Vega <j.vegav@uniandes.edu.co>
2024-09-17 08:16:28 -0500

    commit inicial

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Post from "./Post";
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Container className="mt-3">
+    <Row>
+      <Post author="John Doe" content="This is the post content" likes={20} />
+      <Post author="Anne Hill" content="I like React" likes={3} />
+      <Post author="Laia Martins" content="I love JSX" likes={0} />
+    </Row>
+  </Container>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
